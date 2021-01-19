40d4bc120dd17e02407087f6b3d544edd06887be
Natalia Kazakova (DevExpress) <natalia.kazakova@devexpress.com>
2021-01-19 10:35:05 +0300

    add client

---
+++ dashboard-vue-app/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <DxDashboardControl 
+            style="height:900px; display: 'block'; width: '100%';"
+            endpoint="http://localhost:5000/api/dashboard"
+        />
+    </div>  
+</template>
+
+<script>
+import { DxDashboardControl } from 'devexpress-dashboard-vue';
+
+export default {
+    components: {
+        DxDashboardControl
+    }
+}
+</script>
---
+++ dashboard-vue-app/src/main.js
@@ -0,0 +1,10 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import "@devexpress/analytics-core/dist/css/dx-analytics.common.css";
+import "@devexpress/analytics-core/dist/css/dx-analytics.light.css";
+import "@devexpress/analytics-core/dist/css/dx-querybuilder.css";
+import "devexpress-dashboard/dist/css/dx-dashboard.light.css";
+
+createApp(App).mount('#app')
