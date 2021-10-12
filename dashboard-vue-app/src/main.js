a91d0a139699b47803f828ab803ba41d88b7e692
Natalia Kazakova <17986517+natakazakova@users.noreply.github.com>
2021-10-12 16:24:21 +0300

    Update main.js

--- dashboard-vue-app/src/main.js
+++ dashboard-vue-app/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import "@devexpress/analytics-core/dist/css/dx-analytics.common.css";
 import "@devexpress/analytics-core/dist/css/dx-analytics.light.css";
