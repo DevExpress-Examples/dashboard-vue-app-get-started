300e395d0a50e6e3480af8aec3a2262ea443222f
Polina Tyureva <polina.tyureva@devexpress.com>
2025-01-24 15:41:27 +0400

    update example

--- dashboard-vue-app/src/App.vue
+++ dashboard-vue-app/src/App.vue
@@ -2,7 +2,7 @@
     <div>
         <DxDashboardControl 
             style="height:900px; display: 'block'; width: '100%';"
-            endpoint="http://localhost:5000/api/dashboard"/>
+            endpoint="https://demos.devexpress.com/services/dashboard/api"/>
     </div>  
 </template>
 
